1dbfb36d7d0286300fa56d81fb031ee356ac71f4
zavanton123 <zavanton123@gmail.com>
2018-10-23 15:29:14 +0300

    Empty activity

--- app/src/main/java/com/zavanton123/mydemokotlin/StudentActivity.kt
+++ app/src/main/java/com/zavanton123/mydemokotlin/StudentActivity.kt
@@ -1,26 +1,15 @@
 package com.zavanton123.mydemokotlin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import org.jetbrains.anko.toast
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_student.*
 
-class StudentActivity : AppCompatActivity(), StudentAdapter.StudentClickListener {
+class StudentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student)
 
-        val students = listOf("Mike", "Jack", "Jane")
-
-        val studentRecyclerView = findViewById<RecyclerView>(R.id.student_recycler_view)
-        studentRecyclerView.layoutManager = LinearLayoutManager(this)
-        studentRecyclerView.adapter = StudentAdapter(this, students)
-    }
-
-    override fun onStudentClick(student: String) {
-
-        toast("Clicked: $student")
+        greeting_text_view.text = getString(R.string.hello_kotlin)
     }
 }
